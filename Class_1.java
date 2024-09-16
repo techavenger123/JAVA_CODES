90736eca328c307f4c0158b7faba535efc242231
MIHIR MITHANI <157868082+techavenger123@users.noreply.github.com>
2024-09-16 21:12:37 +0530

    Update

---
+++ Class_1.java
@@ -0,0 +1,17 @@
+// Java Program for class example
+
+class Student {
+    // data member (also instance variable)
+    int id;
+    // data member (also instance variable)
+    String name;
+
+    public static void main(String args[])
+    {
+        // creating an object of
+        // Student
+        Student s1 = new Student();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
