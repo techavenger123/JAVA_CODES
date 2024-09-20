a66c3c2372898e71206771b3aaf9fc037f8d8515
MIHIR MITHANI <157868082+techavenger123@users.noreply.github.com>
2024-09-20 22:23:19 +0530

    Add files via upload

---
+++ Sample1.java
@@ -0,0 +1,29 @@
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		int a = 0;
+		int b=1;
+		
+		
+		System.out.println(a+ " "+ b);
+		for(int i = 3; i<=n;i++) {
+			int c= a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+			
+		}
+			
+			
+			
+		
+		
+		
+		
+
+	}
+
+}
